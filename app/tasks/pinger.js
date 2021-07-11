33e33005728b5524482d3b9d16b939a1603de21e
k03mad <k03.mad@gmail.com>
2021-07-11 16:52:38 +0300

    update pinger

--- app/tasks/pinger.js
+++ app/tasks/pinger.js
@@ -2,22 +2,6 @@
 
 const {mikrotik} = require('../../env');
 const {pinger} = require('@k03mad/utils');
-const {tcpPingPort} = require('tcp-ping-port');
 
-let lastCheck;
-
-/***/
-module.exports = async () => {
-    const options = [[mikrotik.domain, 9595]];
-
-    await Promise.all(options.map(async option => {
-        const {online} = await tcpPingPort(...option);
-
-        if (lastCheck !== online) {
-            const status = online ? 'UP' : 'DOWN';
-            await pinger.notify({text: `\`${status}\` ${option.join(':')} (from cloud)`});
-
-            lastCheck = online;
-        }
-    }));
-};
+/** @returns {Promise} */
+module.exports = () => pinger.check({domain: mikrotik.domain, port: 9595});
