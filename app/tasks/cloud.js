93204dc522b548d9ac8c29402a79a06f55b1576c
k03mad <k03.mad@gmail.com>
2021-07-20 18:34:40 +0300

    update f2b

--- app/tasks/cloud.js
+++ app/tasks/cloud.js
@@ -11,19 +11,30 @@ module.exports = async () => {
     const memory = {};
     const cpu = {};
     const restarts = {};
+    const banned = {};
+
+    const f2b = {
+        grafana: 'sudo fail2ban-client status grafana',
+        sshd: 'sudo fail2ban-client status sshd',
+    };
 
     const uptime = await shell.run('uptime');
     const disk = await shell.run('df');
     const ram = await shell.run('free -m');
-    const log = await shell.run('pm2 jlist');
-    const banned = await shell.run('sudo fail2ban-client status sshd');
+    const pm2 = await shell.run('pm2 jlist');
+
     const cacheFiles = await globby(path.join(os.tmpdir(), hasha('').slice(0, 10)));
 
-    JSON.parse(log).forEach(elem => {
+    JSON.parse(pm2).forEach(elem => {
         memory[elem.name] = elem.monit.memory;
         cpu[elem.name] = elem.monit.cpu;
         restarts[elem.name] = elem.pm2_env.restart_time;
     });
+
+    await Promise.all(Object.entries(f2b).map(async ([key, value]) => {
+        const log = await shell.run(value);
+        banned[key] = Number(log.match(/Currently banned:\s+(\d+)/)[1]);
+    }));
 
     const usage = {
         ramUsage: Number(ram.match(/Mem: +\d+ +(\d+)/)[1]),
@@ -39,6 +50,6 @@ module.exports = async () => {
         {meas: 'cloud-node-memory', values: memory},
         {meas: 'cloud-node-restarts', values: restarts},
         {meas: 'cloud-usage', values: usage},
-        {meas: 'cloud-banned', values: usage},
+        {meas: 'cloud-banned', values: banned},
     ]);
 };
