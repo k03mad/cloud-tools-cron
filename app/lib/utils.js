8e6f50322853bdc25e8b5f4e6774542dd0e17f96
k03mad <k03.mad@gmail.com>
2022-04-14 15:02:15 +0300

    update replace

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -3,29 +3,43 @@
  * @returns {string}
  */
 export const renameIsp = isp => {
+    const ispClone = isp;
+
     const replaces = [
         ['Net By Net Holding LLC', 'NBN'],
+
         [/T2 Mobile|Tele2 Russia/, 'Tele2'],
+        ['PVimpelCom', 'Beeline'],
+
         ['YANDEX', 'Yandex'],
         ['CLOUDFLARENET', 'Cloudflare'],
 
-        ['AO'],
-        ['Bank'],
-        ['Company'],
-        ['Liability'],
-        ['Limited'],
-        ['LLC'],
-        ['OOO'],
+        'AO',
+        'Bank',
+        'Communications',
+        'Company',
+        'GmbH',
+        'incorporated',
+        'Liability',
+        'Limited',
+        'LLC',
+        'ltd.',
+        'OOO',
 
-        [/incorporated/i],
-        [/ltd.?/i],
-        [/Oy$/],
-        [/P?JSC/],
+        /Oy$/,
+        /P?JSC/,
     ];
 
-    replaces.forEach(([from, to = '']) => {
+    replaces.forEach(elem => {
+        let from = elem;
+        let to = '';
+
+        if (Array.isArray(elem)) {
+            [from, to] = elem;
+        }
+
         isp = isp.replace(from, to);
     });
 
-    return isp.trim();
+    return isp.trim() || ispClone;
 };
