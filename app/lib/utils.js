c6b7bd841c60c506849c4067c7d555ffd3df6cb9
k03mad <k03.mad@gmail.com>
2022-04-22 00:08:50 +0300

    return replace

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,19 +1,28 @@
+import {array} from '@k03mad/util';
+
 /**
  * @param {string} isp
  * @returns {string}
  */
 export const renameIsp = isp => {
     const replaces = [
-        ['Net By Net Holding LLC', 'NBN'],
-        ['PVimpelCom', 'Beeline'],
-        ['CLOUDFLARENET', 'Cloudflare'],
+        [/.*\scomcor$/i, 'Akado'],
+        [/^cloudflarenet$/i, 'Cloudflare'],
+        [/^megafon$/i, 'Megafon'],
+        [/^net by net holding llc$/i, 'NBN'],
+        [/^pvimpelcom$/i, 'Beeline'],
+        [/^yandex$/i, 'Yandex'],
+
+        /\sllc/i,
+        /\sltd\.?/i,
+        /\soy$/i,
+        /p?jsc\s/i,
     ];
 
-    for (const [from, to] of replaces) {
-        if (isp.trim() === from) {
-            return to;
-        }
-    }
+    replaces.forEach(elem => {
+        const [from, to = ''] = array.convert(elem);
+        isp = isp.replace(from, to);
+    });
 
-    return isp;
+    return isp.trim();
 };
