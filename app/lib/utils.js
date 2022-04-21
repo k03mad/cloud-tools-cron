c28bb5523f5cad55f2464be94fcc54576daa93f5
k03mad <k03.mad@gmail.com>
2022-04-21 17:09:30 +0300

    simple rename isp

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -3,43 +3,17 @@
  * @returns {string}
  */
 export const renameIsp = isp => {
-    const ispClone = isp;
-
     const replaces = [
         ['Net By Net Holding LLC', 'NBN'],
-
-        [/T2 Mobile|Tele2 Russia/, 'Tele2'],
         ['PVimpelCom', 'Beeline'],
-
-        ['YANDEX', 'Yandex'],
         ['CLOUDFLARENET', 'Cloudflare'],
-
-        'AO',
-        'Bank',
-        'Communications',
-        'Company',
-        'GmbH',
-        'incorporated',
-        'Liability',
-        'Limited',
-        'LLC',
-        'ltd.',
-        'OOO',
-
-        /Oy$/,
-        /P?JSC/,
     ];
 
-    replaces.forEach(elem => {
-        let from = elem;
-        let to = '';
-
-        if (Array.isArray(elem)) {
-            [from, to] = elem;
+    for (const [from, to] of replaces) {
+        if (isp.trim() === from) {
+            return to;
         }
+    }
 
-        isp = isp.replace(from, to);
-    });
-
-    return isp.trim() || ispClone;
+    return isp;
 };
