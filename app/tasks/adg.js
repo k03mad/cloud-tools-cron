f14c088d8427a83f0f9024d9b553d04af58c220d
k03mad <k03.mad@gmail.com>
2022-05-02 20:24:58 +0300

    filter android dns

--- app/tasks/adg.js
+++ app/tasks/adg.js
@@ -151,6 +151,7 @@ export default async () => {
             && object.count(logsBlockedDomainsValues, item.request.domain);
 
         item.response.action_status === 'REQUEST_ALLOWED'
+            && !item.request.domain.endsWith('-dnsotls-ds.metric.gstatic.com')
             && object.count(logsAllowedDomainsValues, item.request.domain);
 
         item.response.action_status === 'RESPONSE_BLOCKED'
