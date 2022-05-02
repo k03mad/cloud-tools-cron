211b0ba77db96970a1afcbbf7bb0fd3eb401ab06
k03mad <k03.mad@gmail.com>
2022-05-02 13:27:35 +0300

    remove magnet

--- app/tasks/pinger.js
+++ app/tasks/pinger.js
@@ -7,7 +7,6 @@ const {adg, cloud, mikrotik} = env;
 /** @returns {Promise} */
 export default () => pinger.check([
     {domain: cloud.domain, port: cloud.grafana.port},
-    {domain: cloud.domain, port: cloud.magnet.port},
     {domain: mikrotik.host, port: mikrotik.port.api},
     {domain: adg.domain, port: 853},
     {domain: 'dns.adguard.com', port: 53},
